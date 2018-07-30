889437256df9e9b9b2a1d6656e97e3a26d006ce3
cnyaw <wayncc@gmail.com>
2018-07-31 07:22:07 +0800

    Added basic bet loop.

--- script/racing.lua
+++ script/racing.lua
@@ -1,22 +1,28 @@
 
 local PREPARE_TIME = 3
+local MAX_SAI_HOON = 5
 local SAI_HOON_SCALE = 1.75
 local COLOR = {0xffff00ff, 0xffff0000, 0xff00ff00, 0xff0000ff, 0xffffff00}
 
 local tex_sandglass_id = 273
 local sai_hoon_obj_id = 335
 
-local sai_hoon_sz
+local sai_hoon_sz = 0
+
+local bet_coin = {}
+local bet_dummy = nil
+local bet_coin_obj = {}
 
 RacingGame = {}
 
 RacingGame.OnCreate = function(param)
+  bet_dummy = nil
   UpdateCoinInfo(param)
   param.stage = OnRacingGameInitStep
 end
 
 RacingGame.OnStep = function(param)
-  if (Input.IsKeyPushed(Input.LBUTTON)) then
+  if (Input.IsKeyPressed(Input.ESCAPE)) then
     Good.GenObj(-1, MAIN_MAP_LVL_ID)
     return
   end
@@ -40,7 +46,7 @@ OnRacingGameInitStep = function(param)
   param.dummy = dummy
   param.count_down = nil
   if (nil ~= param.h) then
-    for i = 1, #param.h do
+    for i = 1, MAX_SAI_HOON do
       Good.KillObj(param.h[i])
     end
     param.h = nil
@@ -48,13 +54,19 @@ OnRacingGameInitStep = function(param)
   local w,h = Resource.GetSpriteSize(sai_hoon_obj_id)
   sai_hoon_sz = h * SAI_HOON_SCALE
   param.h = {}
-  for i = 1,5 do
+  for i = 1, MAX_SAI_HOON do
     local h = Good.GenObj(-1, sai_hoon_obj_id)
     Good.SetBgColor(h, COLOR[i])
     Good.SetScale(h, SAI_HOON_SCALE, SAI_HOON_SCALE)
     Good.SetPos(h, 0, (sai_hoon_sz + 5) * i)
     param.h[i] = h
   end
+  if (nil ~= bet_dummy) then
+    Good.KillObj(bet_dummy)
+  end
+  bet_dummy = Good.GenDummy(-1)
+  bet_coin = {}
+  bet_coin_obj = {}
   param.stage = OnRacingGamePrepareStep
 end
 
@@ -69,6 +81,7 @@ OnRacingGamePrepareStep = function(param)
     return
   end
 
+  -- Hour glass count down.
   local n = math.floor(param.cd / 60) + 1
   if (nil == param.count_down or param.count_down_n ~= n) then
     if (nil ~= param.count_down) then
@@ -81,17 +94,52 @@ OnRacingGamePrepareStep = function(param)
     param.count_down = o
     param.count_down_n = n
   end
+
+  -- Handle bet.
+  if (0 >= curr_coin) then              -- No coin.
+    return
+  end
+
+  if (not Input.IsKeyPushed(Input.LBUTTON)) then
+    return
+  end
+
+  local x, y = Input.GetMousePos()
+  for i = 1, MAX_SAI_HOON do
+    if (PtInObj(x, y, param.h[i])) then
+      if (nil == bet_coin[i]) then
+        bet_coin[i] = 1
+      else
+        bet_coin[i] = bet_coin[i] + 1
+      end
+      if (nil ~= bet_coin_obj[i]) then
+        Good.KillObj(bet_coin_obj[i])
+        bet_coin_obj[i] = nil
+      end
+      local o = GenNumObj(bet_coin[i], 32)
+      Good.AddChild(bet_dummy, o)
+      Good.SetPos(o, Good.GetPos(param.h[i]))
+      bet_coin_obj[i] = o
+      curr_coin = curr_coin - 1
+      UpdateCoinInfo(param)
+      break
+    end
+  end
 end
 
 OnRacingGameRunStep = function(param)
   local touch_end = false
-  for i = 1, #param.h do
+  for i = 1, MAX_SAI_HOON do
     local o = param.h[i]
     local x, y = Good.GetPos(o)
     x = x + math.random() * math.random(1,2)
     Good.SetPos(o, x, y)
     touch_end = SCR_W - sai_hoon_sz <= x
     if (touch_end) then
+      if (nil ~= bet_coin[i]) then
+        curr_coin = curr_coin + 2 * bet_coin[i]
+        UpdateCoinInfo(param)
+      end
       Good.SetScript(o, 'AnimTalkArrow')
       param.cd = 3 * 60
       param.stage = OnRacingGameEndStep
