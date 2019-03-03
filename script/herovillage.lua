47bea8702d09dd0ab1488646359baf016662f811
cnyaw <wayncc@gmail.com>
2019-03-04 06:54:38 +0800

    Back to title if press ESC key in main map or hero village.

--- script/herovillage.lua
+++ script/herovillage.lua
@@ -7,6 +7,10 @@ HeroVillage.OnCreate = function(param)
 end
 
 HeroVillage.OnStep = function(param)
+  if (Input.IsKeyPressed(Input.ESCAPE)) then
+    Good.GenObj(-1, TITLE_LVL_ID)
+    return
+  end
   if (Input.IsKeyPushed(Input.LBUTTON)) then
     local x, y = Input.GetMousePos()
     QuestOnStep(x, y)
--- script/mainmap.lua
+++ script/mainmap.lua
@@ -8,6 +8,10 @@ MainMap.OnCreate = function(param)
 end
 
 MainMap.OnStep = function(param)
+  if (Input.IsKeyPressed(Input.ESCAPE)) then
+    Good.GenObj(-1, TITLE_LVL_ID)
+    return
+  end
   if (Input.IsKeyPushed(Input.LBUTTON)) then
     local x, y = Input.GetMousePos()
     QuestOnStep(x, y)
