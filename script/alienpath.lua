064160639c78e4eb1b27d7f88f1a04dd6c918c6d
cnyaw <wayncc@gmail.com>
2019-03-07 07:22:22 +0800

    Show weapon icons to alien path scene.

--- script/alienpath.lua
+++ script/alienpath.lua
@@ -6,26 +6,13 @@ local paper_tex_id = 358
 local scissor_tex_id = 359
 local stone_tex_id = 360
 
+local weapon_tex_id = {paper_tex_id, scissor_tex_id, stone_tex_id}
+
 AlienPath = {}
 
 AlienPath.OnCreate = function(param)
-  local tex_id = {paper_tex_id, scissor_tex_id, stone_tex_id}
-  local can_hit = {false, false, HasBackScratcher()}
-  param.hit = 0
-  param.obj = {}
-  local w, h = Resource.GetTexSize(ufo_tex_id)
-  for i = 1, MAX_ALIEN do
-    local o = Good.GenObj(-1, ufo_tex_id, 'BouncingObj')
-    local x = math.random(SCR_W - w)
-    local y = math.random(SCR_H - h)
-    Good.SetPos(o, x, y)
-    local index = 1 + (i % #tex_id)
-    local rps = Good.GenObj(o, tex_id[index])
-    local sw, sh = ScaleToSize(rps, RPS_SIZE, RPS_SIZE)
-    Good.SetPos(rps, (w - sw)/2, h - sh)
-    Good.GetParam(o).can_hit = can_hit[index]
-    param.obj[i] = o
-  end
+  GenAliens(param)
+  GenWeaponIcons()
   QuestOnCreate()
 end
 
@@ -64,4 +51,43 @@ end
 function CanHitAlien(o)
   local param = Good.GetParam(o)
   return param.can_hit
+end
+
+function GenAliens(param)
+  local can_hit = {false, false, HasBackScratcher()}
+  param.hit = 0
+  param.obj = {}
+  local w, h = Resource.GetTexSize(ufo_tex_id)
+  for i = 1, MAX_ALIEN do
+    local o = Good.GenObj(-1, ufo_tex_id, 'BouncingObj')
+    local x = math.random(SCR_W - w)
+    local y = math.random(SCR_H - h)
+    Good.SetPos(o, x, y)
+    local index = 1 + (i % #weapon_tex_id)
+    local rps = Good.GenObj(o, weapon_tex_id[index])
+    local sw, sh = ScaleToSize(rps, RPS_SIZE, RPS_SIZE)
+    Good.SetPos(rps, (w - sw)/2, h - sh)
+    Good.GetParam(o).can_hit = can_hit[index]
+    param.obj[i] = o
+  end
+end
+
+function GenWeaponIcon(x, y, tex_id, has)
+  local o = Good.GenObj(-1, tex_id)
+  Good.SetPos(o, x, y)
+  local sz = RPS_SIZE / 1.5
+  ScaleToSize(o, sz, sz)
+  if (not has) then
+    Good.SetAlpha(o, 100)
+  end
+  x = x + sz + 2
+  return x, y
+end
+
+function GenWeaponIcons()
+  local has = {HasBackScratcher(), false, false}
+  local x, y = 0, 0
+  for i = 1, #weapon_tex_id do
+    x, y = GenWeaponIcon(x, y, weapon_tex_id[i], has[i])
+  end
 end
