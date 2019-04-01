64605c29bb6c83b3cfbd52a8df08bce2e8a1dcd2
cnyaw <wayncc@gmail.com>
2019-04-02 06:43:32 +0800

    Show right or wrong color to input code.

--- script/cavedoor.lua
+++ script/cavedoor.lua
@@ -1,6 +1,12 @@
 local CX_BLOCK = 30
 local CX_MARGIN = 10
 local CX_BLOCK_MARGIN = (CX_BLOCK + CX_MARGIN)
+local OFFSET_Y = 300
+local COLOR_BLACK = 0xff000000
+local COLOR_GRAY = 0xff808080
+local COLOR_INPUT_RIGHT = 0xff00ff00
+local COLOR_INPUT_WRONG = 0xffff0000
+local WAIT_TIME = 30
 
 local input_code_right_talk_id = 2103
 local add_mallet_talk_id = 2104
@@ -11,6 +17,7 @@ CaveDoor = {}
 CaveDoor.OnCreate = function(param)
   EnterPlace(param._id)
   InitCaveDoor(param)
+  param.step = CaveDoorOnStepInput
 end
 
 CaveDoor.OnStep = function(param)
@@ -18,15 +25,13 @@ CaveDoor.OnStep = function(param)
     Good.GenObj(-1, GetHeroVillageBackLvlId())
     return
   end
-  if (Input.IsKeyPushed(Input.LBUTTON)) then
-    CaveDoorHittest(param)
-  end
+  param.step(param)
 end
 
 function AddInputCode(param, code)
   param.input_code = param.input_code .. code
   local idx = string.len(param.input_code)
-  Good.SetBgColor(param.block_obj[idx], 0xff808080)
+  Good.SetBgColor(param.block_obj[idx], COLOR_GRAY)
 end
 
 function CaveDoorHittest(param)
@@ -40,9 +45,30 @@ function CaveDoorHittest(param)
   end
 end
 
+function CaveDoorOnStepInput(param)
+  if (Input.IsKeyPushed(Input.LBUTTON)) then
+    CaveDoorHittest(param)
+  end
+end
+
+function CaveDoorOnStepInputRight(param)
+  if (not WaitTimer(param, WAIT_TIME)) then
+    return
+  end
+  StartTalk(GetBingoTalkId())
+end
+
+function CaveDoorOnStepInputWrong(param)
+  if (not WaitTimer(param, WAIT_TIME)) then
+    return
+  end
+  ClearInputCode(param)
+  param.step = CaveDoorOnStepInput
+end
+
 function ClearInputCode(param)
   for i = 1, #param.block_obj do
-    Good.SetBgColor(param.block_obj[i], 0xff000000)
+    Good.SetBgColor(param.block_obj[i], COLOR_BLACK)
   end
   param.input_code = ''
 end
@@ -52,7 +78,7 @@ function CorrectInputCode(s)
 end
 
 function GenInputCodeBlock(param, x, y)
-  local o = GenColorObj(-1, CX_BLOCK, CX_BLOCK, 0xff000000)
+  local o = GenColorObj(-1, CX_BLOCK, CX_BLOCK, COLOR_BLACK)
   Good.SetPos(o, x, y)
   return o
 end
@@ -76,7 +102,6 @@ end
 function InitInputCodeBlock(param)
   local code = GetOpenCaveDoorCode()
   local OFFSET_X = (SCR_W - string.len(code) * CX_BLOCK_MARGIN) / 2
-  local OFFSET_Y = 300
   param.block_obj = {}
   local x = OFFSET_X
   for i = 1, string.len(code) do
@@ -98,12 +123,21 @@ function InputCodeFull(code)
   return string.len(code) == string.len(GetOpenCaveDoorCode())
 end
 
+function SetInputCodeBlockColor(param, color)
+  for i = 1, #param.block_obj do
+    Good.SetBgColor(param.block_obj[i], color)
+  end
+end
+
 function ValidateInputCode(param)
-  if (not CorrectInputCode(param.input_code)) then
-    if (InputCodeFull(param.input_code)) then
-      ClearInputCode(param)
-    end
+  if (not InputCodeFull(param.input_code)) then
     return
   end
-  StartTalk(GetBingoTalkId())
+  if (not CorrectInputCode(param.input_code)) then
+    SetInputCodeBlockColor(param, COLOR_INPUT_WRONG)
+    param.step = CaveDoorOnStepInputWrong
+  else
+    SetInputCodeBlockColor(param, COLOR_INPUT_RIGHT)
+    param.step = CaveDoorOnStepInputRight
+  end
 end
