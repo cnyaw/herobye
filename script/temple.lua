f86c3361306154c3846ccec95a516559dd8638f7
cnyaw <wayncc@gmail.com>
2020-05-18 14:24:06 +0800

    Bet coin by moving user.

--- script/temple.lua
+++ script/temple.lua
@@ -6,7 +6,9 @@ local USER_MOVE_SPEED = 12
 
 local TILE_POS = {0,0, 1,0, 2,0, 3,0, 4,0, 5,0, 6,0, 7,0, 7,1, 7,2, 7,3, 6,3, 5,3,
                   4,3, 3,3, 2,3, 1,3, 0,3, 0,2, 0,1}
+local TILE_FACTOR = {1, 2, 2, 3, 1, 2, 3, 3, -1}
 
+local map_obj_id = 159
 local dice1_obj_id = 171
 local dice2_obj_id = 174
 local user_obj_id = 172
@@ -14,6 +16,7 @@ local user_obj_id = 172
 local user_sw, user_sh
 local user_pos, new_user_pos
 local user_move_time
+local bet_count = 2
 
 local function CalCTilePos(p)
   return ((p - 1) % NUM_TILE_POS) + 1
@@ -22,6 +25,14 @@ end
 local function GetTilePos(p)
   local i = 2 * (p - 1) + 1
   return TILE_SZ * TILE_POS[i], TILE_SZ * TILE_POS[i + 1]
+end
+
+local function GetCurrTileFactor()
+  local x, y = GetTilePos(user_pos)
+  local map = Good.GetMapId(map_obj_id)
+  local tile = Resource.GetTileByPos(map, x, y)
+  local factor = TILE_FACTOR[tile]
+  return factor
 end
 
 local function RandDice(o)
@@ -88,7 +99,9 @@ end
 
 function TempleOnStep(param)
   if (Input.IsKeyPushed(Input.LBUTTON)) then
-    param.step = TempleOnStepRollDice
+    if (GetCoin() >= bet_count) then
+      param.step = TempleOnStepRollDice
+    end
   end
 end
 
@@ -97,6 +110,14 @@ function TempleOnStepMoveUser(param)
     return
   end
   user_pos = new_user_pos
+  local factor = GetCurrTileFactor()
+  local c = factor * bet_count
+  if (0 < c) then
+    AddCoin(c)
+  else
+    ConsumeCoin(-c)
+  end
+  UpdateCoinInfo(param)
   param.step = TempleOnStep
 end
 
@@ -105,6 +126,8 @@ function TempleOnStepRollDice(param)
   if (not WaitTimer(param, WAIT_TIME)) then
     return
   end
+  ConsumeCoin(bet_count)
+  UpdateCoinInfo(param)
   user_move_time = n * USER_MOVE_SPEED
   new_user_pos = CalCTilePos(user_pos + n)
   SetUserMoveToPath()
