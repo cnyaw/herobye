59bf22401a87be97663c7ff4ca16a89d164944cd
cnyaw <wayncc@gmail.com>
2020-05-19 14:32:45 +0800

    Renamed jewel_id to jewel_tex_id.

--- script/pairgame.lua
+++ script/pairgame.lua
@@ -3,17 +3,27 @@ local ROW_COUNT = 3
 local COL_COUNT = 4
 local MARGIN = 20
 
-local jewel_id = 105
+local jewel_tex_id = 105
 
 local COLORS = {COLOR_RED, COLOR_GREEN, COLOR_BLUE}
 local W, H, OFFSET_X, OFFSET_Y, WW, HH
 
 local pass_talk_id = 2501
 
+local function ClearPair(param, i, j)
+  param.colors[i] = -1
+  Good.SetBgColor(param.objs[i], COLOR_BLACK)
+  param.colors[j] = -1
+  Good.SetBgColor(param.objs[j], COLOR_BLACK)
+  param.sel_obj = nil
+  param.count = param.count - 2
+  return 0 == param.count
+end
+
 PairGame = {}
 
 PairGame.OnCreate = function(param)
-  W, H = Resource.GetTexSize(jewel_id)
+  W, H = Resource.GetTexSize(jewel_tex_id)
   OFFSET_X = (SCR_W - (COL_COUNT * (W + MARGIN))) / 2
   OFFSET_Y = (SCR_H - (ROW_COUNT * (H + MARGIN))) / 2
   WW, HH = W + MARGIN, H + MARGIN
@@ -23,7 +33,7 @@ PairGame.OnCreate = function(param)
   local obj_index = color_offset
   for i = 0, ROW_COUNT - 1 do
     for j = 0, COL_COUNT - 1 do
-      local o = Good.GenObj(-1, jewel_id)
+      local o = Good.GenObj(-1, jewel_tex_id)
       Good.SetPos(o, OFFSET_X + j * WW, OFFSET_Y + i * HH)
       local color_index = 1 + obj_index % #COLORS
       Good.SetBgColor(o, COLORS[color_index])
@@ -76,12 +86,3 @@ PairGame.OnStep = function(param)
   end
 end
 
-function ClearPair(param, i, j)
-  param.colors[i] = -1
-  Good.SetBgColor(param.objs[i], COLOR_BLACK)
-  param.colors[j] = -1
-  Good.SetBgColor(param.objs[j], COLOR_BLACK)
-  param.sel_obj = nil
-  param.count = param.count - 2
-  return 0 == param.count
-end
