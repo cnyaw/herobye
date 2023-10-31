38e59335c561362f04988f9856b67b034f7198d7
cnyaw <wayncc@gmail.com>
2023-10-31 11:41:14 +0800

    Moved script functions to module Scope.

--- script/script.lua
+++ script/script.lua
@@ -1,95 +1,97 @@
 
 local SELL_ITEM_COST = 4
 
-function ScriptAddBackScratcher()
+Script = {}
+
+function Script.AddBackScratcher()
   SetItem('i_back_scratcher', 1)
 end
 
-function ScriptAddBou1()
+function Script.AddBou1()
   SetItem('i_bou', 1)
 end
 
-function ScriptAddCaveDoorBook()
+function Script.AddCaveDoorBook()
   SetItem('i_cave_door_book', 1)
 end
 
-function ScriptAddCaveMazeBookFlag()
+function Script.AddCaveMazeBookFlag()
   SetItem('f_cave_maze_book', 1)
   SetItem('f_open_cave_door', 0)
 end
 
-function ScriptAddCaveMazeBook()
+function Script.AddCaveMazeBook()
   RemoveItem('f_cave_maze_book', 1)
   SetItem('i_cave_maze_book', 1)
 end
 
-function ScriptAddDipsyFeather()
+function Script.AddDipsyFeather()
   SetItem('i_dipsy_feather', 1)
 end
 
-function ScriptAddDrumStick()
+function Script.AddDrumStick()
   SetItem('i_drum_stick', 1)
 end
 
-function ScriptAddEarthWorm()
+function Script.AddEarthWorm()
   SetItem('i_earthworm', 1)
 end
 
-function ScriptAddFish()
+function Script.AddFish()
   SetItem('i_fish', 1)
 end
 
-function ScriptAddFrogTear()
+function Script.AddFrogTear()
   SetItem('i_frog_tear', 1)
 end
 
-function ScriptAddHeroHisBook()
+function Script.AddHeroHisBook()
   SetItem('i_hero_history_book', 1)
 end
 
-function ScriptAddJewel()
+function Script.AddJewel()
   SetItem('i_jewel', 1)
 end
 
-function ScriptAddKingPermission()
+function Script.AddKingPermission()
   SetItem('f_king_permission', 1)
 end
 
-function ScriptAddLaaLaaFeather()
+function Script.AddLaaLaaFeather()
   SetItem('i_laalaa_feather', 1)
 end
 
-function ScriptAddMellet()
+function Script.AddMellet()
   SetItem('f_mallet_code', 0)
   SetItem('i_mallet', 1)
 end
 
-function ScriptAddPoFeather()
+function Script.AddPoFeather()
   SetItem('i_po_feather', 1)
 end
 
-function ScriptAddPowerScissorBook()
+function Script.AddPowerScissorBook()
   SetItem('i_power_scissor_book', 1)
 end
 
-function ScriptAddRope()
+function Script.AddRope()
   SetItem('f_need_rope', 0)
   SetItem('i_rope', 1)
 end
 
-function ScriptAddRpsMedal()
+function Script.AddRpsMedal()
   SetItem('i_rps_medal', 1)
 end
 
-function ScriptAddShaveBook()
+function Script.AddShaveBook()
   SetItem('i_shave_book', 1)
 end
 
-function ScriptAddSlapMouseBook()
+function Script.AddSlapMouseBook()
   SetItem('i_slap_mouse_book', 1)
 end
 
-function ScriptAddSuperSoap()
+function Script.AddSuperSoap()
   SetItem('i_frog_tear', 0)
   SetItem('i_green_mouse_tail', 0)
   SetItem('i_red_mouse_tail', 0)
@@ -97,136 +99,136 @@ function ScriptAddSuperSoap()
   SetItem('i_super_soap', 1)
 end
 
-function ScriptAddSuperSoapBook()
+function Script.AddSuperSoapBook()
   SetItem('i_super_soap_book', 1)
 end
 
-function ScriptAddTinkyWinkyFeather()
+function Script.AddTinkyWinkyFeather()
   SetItem('i_tinky_winky_feather', 1)
 end
 
-function ScriptAddUfoPower()
+function Script.AddUfoPower()
   SetItem('i_ufo_power', 1)
   SetItem('f_find_ufo_power', 0)
 end
 
-function ScriptAddUfoPowerBook()
+function Script.AddUfoPowerBook()
   SetItem('i_ufo_power_book', 1)
 end
 
-function ScriptBatCaveBlocked()
+function Script.BatCaveBlocked()
   SetItem('f_bat_cave_blocked', 1)
 end
 
-function ScriptBuildTemple()
+function Script.BuildTemple()
   ConsumeCoin(CROWD_FUNDING_COST)
 end
 
-function ScriptBuyBou2()
+function Script.BuyBou2()
   ConsumeCoin(BOU2_COST)
   SetItem('i_bou2', 1)
 end
 
-function ScriptBuyBou3()
+function Script.BuyBou3()
   ConsumeCoin(BOU3_COST)
   SetItem('i_bou3', 1)
 end
 
-function ScriptBuyCandy()
+function Script.BuyCandy()
   ConsumeCoin(CANDY_COST)
   SetItem('i_candy', 1)
   SetItem('f_buy_candy_loop', 0)
 end
 
-function ScriptBuyCandyLoop()
+function Script.BuyCandyLoop()
   SetItem('f_buy_candy_loop', 1)
 end
 
-function ScriptBuyFlashlight()
+function Script.BuyFlashlight()
   ConsumeCoin(FLASHLIGHT_COST)
   SetItem('i_flashlight', 1)
   SetItem('i_flashlight_use_count', MAX_FLASH_LIGHT_USE_COUNT)
 end
 
-function ScriptBuyShip()
+function Script.BuyShip()
   ConsumeCoin(SHIP_COST)
   SetItem('f_ship', 1)
 end
 
-function ScriptChargeFlashlight()
+function Script.ChargeFlashlight()
   ScriptBuyFlashlight()
 end
 
-function ScriptCleanYellowSuit()
+function Script.CleanYellowSuit()
   SetItem('i_super_soap', 0)
   SetItem('i_dirty_yellow_suit', 0)
   SetItem('i_clean_yellow_suit', 1)
 end
 
-function ScriptConsumeUfoPower()
+function Script.ConsumeUfoPower()
   SetItem('i_ufo_power', 0)
 end
 
-function ScriptEnterCave()
+function Script.EnterCave()
   AddItem('i_enter_cave_count', 1)
 end
 
-function ScriptExchangeCandyScissor()
+function Script.ExchangeCandyScissor()
   SetItem('i_candy', 0)
   SetItem('i_scissor', 1)
 end
 
-function ScriptFeedEarthWormToBird()
+function Script.FeedEarthWormToBird()
   SetItem('i_earthworm', 0)
 end
 
-function ScriptFindTeletubbies()
+function Script.FindTeletubbies()
   SetItem('f_find_teletubbies', 1)
 end
 
-function ScriptFindUfoPower()
+function Script.FindUfoPower()
   SetItem('f_find_ufo_power', 1)
 end
 
-function ScriptGetMalletCode()
+function Script.GetMalletCode()
   SetItem('f_mallet_code', 1)
 end
 
-function ScriptGiveAllowance()
+function Script.GiveAllowance()
   AddCoin(10)
 end
 
-function ScriptGiveGodzilla()
+function Script.GiveGodzilla()
   SetItem('i_godzilla', 1);
 end
 
-function ScriptHelpElderGlass()
+function Script.HelpElderGlass()
   SetItem('f_help_elder_glass', 1)
 end
 
-function ScriptHelpElderWhite()
+function Script.HelpElderWhite()
   SetItem('f_help_elder_white', 1)
 end
 
-function ScriptHelpElderYellow()
+function Script.HelpElderYellow()
   SetItem('f_help_elder_yellow', 1)
   SetItem('i_dirty_yellow_suit', 1)
 end
 
-function ScriptHelpElderGlassDone()
+function Script.HelpElderGlassDone()
   SetItem('i_super_nano_towel', 0)
 end
 
-function ScriptHelpElderYellowDone()
+function Script.HelpElderYellowDone()
   SetItem('i_clean_yellow_suit', 0)
 end
 
-function ScriptHeroMtNextId()
+function Script.HeroMtNextId()
   local id = {51, 180, 280}
   return id[math.random(3)]
 end
 
-function ScriptMakeSuperNanoTowel()
+function Script.MakeSuperNanoTowel()
   SetItem('i_dipsy_feather', 0)
   SetItem('i_laalaa_feather', 0)
   SetItem('i_po_feather', 0)
@@ -235,23 +237,23 @@ function ScriptMakeSuperNanoTowel()
   SetItem('i_super_nano_towel', 1)
 end
 
-function ScriptOpenBatCave()
+function Script.OpenBatCave()
   SetItem('f_bat_cave_blocked', 0)
 end
 
-function ScriptOpenCaveDoor()
+function Script.OpenCaveDoor()
   SetItem('f_open_cave_door', 1)
 end
 
-function ScriptPassPotatoTest()
+function Script.PassPotatoTest()
   SetItem('f_pass_potato_test', 1)
 end
 
-function ScriptPlayLottery()
+function Script.PlayLottery()
   ConsumeCoin(LOTTERY_COST)
 end
 
-function ScriptPowerupScissor()
+function Script.PowerupScissor()
   RemoveItem('i_scissor', 1)
   RemoveItem('i_drum_stick', 1)
   RemoveItem('i_frog_tear', 1)
@@ -259,36 +261,36 @@ function ScriptPowerupScissor()
   SetItem('i_power_scissor', 1)
 end
 
-function ScriptPowerupScissorLoop()
+function Script.PowerupScissorLoop()
   SetItem('f_powerup_scissor', 1)
 end
 
-function ScriptSellEarthWorm()
+function Script.SellEarthWorm()
   SetItem('i_earthworm', 0)
   AddCoin(2 * SELL_ITEM_COST)
 end
 
-function ScriptSellFeather()
+function Script.SellFeather()
   local item = {'i_po_feather', 'i_laalaa_feather', 'i_tinky_winky_feather', 'i_dipsy_feather'}
   ScriptSellItems_i(item, 2 * SELL_ITEM_COST)
 end
 
-function ScriptSellFish()
+function Script.SellFish()
   SetItem('i_fish', 0)
   AddCoin(5 * SELL_ITEM_COST)
 end
 
-function ScriptSellFishToFishman()
+function Script.SellFishToFishman()
   SetItem('i_fish', 0)
   AddCoin(SELL_ITEM_COST)
 end
 
-function ScriptSellFrogTear()
+function Script.SellFrogTear()
   SetItem('i_frog_tear', 0)
   AddCoin(SELL_ITEM_COST)
 end
 
-function ScriptSellItems_i(item, num)
+function Script.SellItems_i(item, num)
   for i = 1, #item do
     if (HasItem(item[i])) then
       SetItem(item[i], 0)
@@ -297,38 +299,38 @@ function ScriptSellItems_i(item, num)
   end
 end
 
-function ScriptSellJewel()
+function Script.SellJewel()
   SetItem('i_jewel', 0)
   AddCoin(3 * SELL_ITEM_COST)
 end
 
-function ScriptSellMouseTail()
+function Script.SellMouseTail()
   local item = {'i_green_mouse_tail', 'i_red_mouse_tail', 'i_yellow_mouse_tail'}
   ScriptSellItems_i(item, SELL_ITEM_COST)
 end
 
-function ScriptSellSuperNanoTowel()
+function Script.SellSuperNanoTowel()
   SetItem('i_super_nano_towel', 0)
   AddCoin(6 * SELL_ITEM_COST)
 end
 
-function ScriptSendTeacherLetter()
+function Script.SendTeacherLetter()
   SetItem('i_letter', 1)
 end
 
-function ScriptSetNeedRopeFlag()
+function Script.SetNeedRopeFlag()
   SetItem('f_need_rope', 1)
 end
 
-function ScriptTakeJewel()
+function Script.TakeJewel()
   SetItem('i_jewel', 0)
 end
 
-function ScriptTempleCreated()
+function Script.TempleCreated()
   SetItem('i_temple_lvl', 1)
 end
 
-function ScriptTextAddMouseTail(fmt)
+function Script.TextAddMouseTail(fmt)
   local i = 1
   if (HasItem('i_red_mouse_tail')) then
     i = 2
@@ -347,19 +349,19 @@ function ScriptTextAddMouseTail(fmt)
   return string.format(fmt, s[i])
 end
 
-function ScriptTextCrowdFunding(fmt)
+function Script.TextCrowdFunding(fmt)
   return string.format(fmt, CROWD_FUNDING_COST - GetCoin())
 end
 
-function ScriptTextFlashlightUseCount(fmt)
+function Script.TextFlashlightUseCount(fmt)
   return string.format(fmt, FlashlightUseCount())
 end
 
-function ScriptTransLetterToPriest()
+function Script.TransLetterToPriest()
   ConsumeCoin(CHURCH_RECV_LETTER_COST)
   RemoveItem('i_letter', 1)
 end
 
-function ScriptUseFlashlight()
+function Script.UseFlashlight()
   RemoveItem('i_flashlight_use_count', 1)
 end
